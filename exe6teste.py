351c1bdd76b3c8a05367c19802b74672ad7b509d
sabrinarprado <sabryna13byna@gmail.com>
2025-01-29 13:16:23 -0300

    Exercício 6 - resolução alternativa

---
+++ exe6teste.py
@@ -0,0 +1,19 @@
+def numero_fibonacci(n):
+    a = 0
+    b = 1
+    while a <= n:
+        if a == n:
+            return True
+        
+        temp = a
+        a = b
+        b = temp + b
+      
+        
+    return False
+
+num = int(input("Digite um número: "))
+if numero_fibonacci(num):
+    print(f"O número {num} pertence a sequência Fibonacci.")
+else:
+    print(f"O número {num} não pertence a sequência Fibonacci.")
